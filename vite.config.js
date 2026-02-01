10b78e7d39c5851bf79835ab57ff2f03e1092cab
NaiKiDEV <pc999111@gmail.com>
2026-02-02 00:01:16 +0200

    feat: fix workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/local-llm-landing-page/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
